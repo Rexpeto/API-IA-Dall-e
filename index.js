86dc21fef2dba99fa8a28ff58a4a16e21676394e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-15 15:24:00 -0400

    Use controller dalle

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import dalleRoute from "./routes/dalle.route.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/v1/dalle", dalleRoute);
 
 app.listen(PORT, () => console.log(`Running through the port ${PORT}`));
